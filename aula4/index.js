f204404ea55b3d5a04fbe09c55b8a68016d9e01f
ELIAS FONSECA MARTINS <eliasmartinsdev8@gmail.com>
2022-10-15 12:11:33 -0300

    coloquei um console
    coloquei la o negocio

--- aula4/index.js
+++ aula4/index.js
@@ -1,10 +1,48 @@
-const fs = require('fs').promises;
-const path = require('path');
+ const fs = require('fs').promises;
+ const path = require('path');
 
-// fs.readdir(path.resolve(__dirname))
-//   .then(files=>console.log(files))
-//   .catch(e=>console.log(e))
-async function readdir(){
-  rootDir= rootDir || path.resolve(__dirname);
-  const file = await fs.readdir(rootDir);
-}
+ async function readdir(rootDir){
+   rootDir= rootDir || path.resolve(__dirname);
+   const files = await fs.readdir(rootDir);
+   walk(files)
+ }
+
+ async function walk(files,readdir){
+   for(let file of files){
+     const fileFullPath = path.resolve(__dirname);
+     const stats=await fs.stat( fileFullPath);
+     console.log(file);
+   }
+ }
+readdir('C:\\Users\\elias\\Desktop\\node.js\\node.js\\')
+
+// const fs = require('fs').promises;
+// const path = require('path');
+
+// async function readdir(rootDir) {
+//   rootDir = rootDir || path.resolve(__dirname);
+//   const files = await fs.readdir(rootDir);
+//   walk(files, rootDir);
+// }
+
+// async function walk(files, rootDir) {
+//   for(let file of files) {
+//     const fileFullPath = path.resolve(rootDir, file);
+//     const stats = await fs.stat(fileFullPath);
+
+//     if (/\.git/g.test(fileFullPath)) continue;
+//     if (/node_modules/g.test(fileFullPath)) continue;
+
+//     if(stats.isDirectory()) {
+//       readdir(fileFullPath);
+//       continue;
+//     }
+
+//     if (
+//       !/\.html$/g.test(fileFullPath)
+//     ) continue;
+//     console.log(fileFullPath);
+//   }
+// }
+
+console.log('Biscoito');
